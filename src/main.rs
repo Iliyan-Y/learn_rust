2bba8ea81453345b7e923d43d41651c17b78db72
Iliyan-Y <maximizebg@gmail.com>
2022-09-19 11:30:25 +0100

    data types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
 
+use rand::{random, Rng};
+
 fn main() {
-    get_input();
-    variables();
+    // get_input();
+    // variables();
+    data_types();
 }
 
 fn get_input() {
@@ -22,4 +25,30 @@ fn variables() {
     let mut age: u32 = age.trim().parse().expect("age wsn't assigned a number");
     age = age + 1;
     println!("I'm {} and I want ${}", age, ONE_MILE);
+}
+
+fn data_types() {
+    // Unsigned integer (can be only zero or positive) : u8 u16 u32 u64 u128, usize
+    // Signed integer (can be + 0 -): i8 i16 i32 i64 i128, isize
+
+    // println!("Max u32: {}", u32::MAX);
+    // println!("Max u64: {}", u64::MAX);
+    // println!("Max usize: {}", usize::MAX);
+    // println!("Max u128: {}", u128::MAX);
+    // println!("Max f32: {}", f32::MAX);
+    // println!("Max f64: {}", f64::MAX);
+
+    // underscore in front of variable ignore unused warning
+    let _is_true: bool = true;
+
+    //Math
+
+    let random_number: i32 = rand::thread_rng().gen_range(1..1000);
+    let random_number_2: i32 = rand::thread_rng().gen_range(1..1000);
+    println!(
+        "MATH RANDOM: {}, + {} = {}",
+        random_number,
+        random_number_2,
+        random_number + random_number_2
+    )
 }
